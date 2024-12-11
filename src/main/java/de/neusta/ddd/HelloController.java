2e315f2d0ae0a04f27148b40726f1dc89bac24e8
slohmann <s.lohmann@neusta.de>
2024-12-11 13:34:12 +0100

    fix

--- src/main/java/de/neusta/ddd/HelloController.java
+++ src/main/java/de/neusta/ddd/HelloController.java
@@ -1,14 +1,12 @@
-package de.neusta.controller;
+package de.neusta.ddd;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class HelloController {
 
-    @GetMapping("hello")
+    @GetMapping("/api/hello")
     public String sayHello() {
         return "Hello World!";
     }
